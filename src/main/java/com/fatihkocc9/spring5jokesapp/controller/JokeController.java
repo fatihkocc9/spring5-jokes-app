5bc00b0ab91e31f53092018dcb2434572892af1d
fatihkocc9 <fatihkoc@hotmail.it>
2021-08-26 15:29:11 +0300

    Finish the joke app

--- src/main/java/com/fatihkocc9/spring5jokesapp/controller/JokeController.java
+++ src/main/java/com/fatihkocc9/spring5jokesapp/controller/JokeController.java
@@ -2,7 +2,6 @@ package com.fatihkocc9.spring5jokesapp.controller;
 
 
 import com.fatihkocc9.spring5jokesapp.service.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
