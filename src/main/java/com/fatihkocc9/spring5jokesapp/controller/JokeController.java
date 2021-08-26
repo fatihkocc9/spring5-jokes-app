a59136c8cca7a43e427774ac29c83f2b706427e6
fatihkocc9 <fatihkoc@hotmail.it>
2021-08-26 14:04:55 +0300

    Services

---
+++ src/main/java/com/fatihkocc9/spring5jokesapp/controller/JokeController.java
@@ -0,0 +1,12 @@
+package com.fatihkocc9.spring5jokesapp.controller;
+
+
+import com.fatihkocc9.spring5jokesapp.service.JokeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JokeController {
+    private final JokeService;
+
+
+}
---
+++ src/main/java/com/fatihkocc9/spring5jokesapp/service/JokeService.java
@@ -0,0 +1,11 @@
+package com.fatihkocc9.spring5jokesapp.service;
+
+
+
+
+public interface JokeService {
+
+        String getJoke();
+
+}
+
