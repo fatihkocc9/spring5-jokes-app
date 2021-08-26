e190aa5052bcc02877e79b9472716639fc54d70e
fatihkocc9 <fatihkoc@hotmail.it>
2021-08-26 14:20:27 +0300

    View added with thymeleaf

--- src/main/java/com/fatihkocc9/spring5jokesapp/controller/JokeController.java
+++ src/main/java/com/fatihkocc9/spring5jokesapp/controller/JokeController.java
@@ -2,11 +2,26 @@ package com.fatihkocc9.spring5jokesapp.controller;
 
 
 import com.fatihkocc9.spring5jokesapp.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
-    private final JokeService;
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
 
 
 }
